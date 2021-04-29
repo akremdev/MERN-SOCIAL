bfcd647e1dc57217523a3d6f5b15452854c0a96f
akremdev <akremdev@gmail.com>
2021-04-30 00:36:55 +0100

    fetch user with userEffect + axios

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -2,9 +2,10 @@ import "./post.css";
 import { MoreVert } from "@material-ui/icons";
 import axios from "axios"
 import { useState, useEffect } from "react";
+import {format} from "timeago.js"
 
 export default function Post({ post }) {
-  const [like,setLike] = useState(post.like)
+  const [like,setLike] = useState(post.likes.length)
   const [isLiked,setIsLiked] = useState(false)
   const [user, setUser] = useState({})
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
@@ -15,7 +16,7 @@ export default function Post({ post }) {
       setUser(res.data)
     };
     fetchUser();
-  }, [])
+  }, [post.userId])
 
   const likeHandler =()=>{
     setLike(isLiked ? like-1 : like+1)
@@ -34,7 +35,7 @@ export default function Post({ post }) {
             <span className="postUsername">
               {user.username}
             </span>
-            <span className="postDate">{post.date}</span>
+            <span className="postDate">{format(post.createdAt)}</span>
           </div>
           <div className="postTopRight">
             <MoreVert />
@@ -42,7 +43,7 @@ export default function Post({ post }) {
         </div>
         <div className="postCenter">
           <span className="postText">{post?.desc}</span>
-          <img className="postImg" src={PF+post.photo} alt="" />
+          <img className="postImg" src={PF+post.img} alt="" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
