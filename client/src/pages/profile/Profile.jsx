cad1e53f4a9ae7a31f4c2cafc871ae15990144d9
akremdev <akremdev@gmail.com>
2021-04-30 01:14:16 +0100

    useParams Hook

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -5,18 +5,20 @@ import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
 import axios from "axios"
 import { useState, useEffect } from "react";
+import {useParams} from "react-router-dom"
 
 export default function Profile() {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   const [user, setUser] = useState({})
+  const username = useParams().username;
 
   useEffect(() => {
     const fetchUser = async () => {
-      const res = await axios.get(`/users?username=Akrem Gharbi`)
+      const res = await axios.get(`/users?username=${username}`)
       setUser(res.data)
     };
     fetchUser();
-  }, [])
+  }, [username])
 
   return (
     <>
@@ -43,7 +45,7 @@ export default function Profile() {
             </div>
           </div>
           <div className="profileRightBottom">
-            <Feed username="Akrem Gharbi" />
+            <Feed username={username} />
             <Rightbar user={user}/>
           </div>
         </div>
